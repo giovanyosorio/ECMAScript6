aeff561c505a1e4946a3cafad2de63caa67afc11
gio <giovany.torres.799@gmail.com>
2023-02-03 17:03:15 -0500

    default params

--- 03-strings.js
+++ 03-strings.js
@@ -13,5 +13,12 @@ saludo("hola","mundo")
 
 //multi-line Strings
 
-let lorem="esto es un string  \n" + " "
+let lorem="esto es un string  \n" + "esto es otra linea "
+//ES6
 
+let lorem2=`Esta es una frase epica
+Esta es la continuacion 
+`
+
+console.log(lorem);
+console.log(lorem2);
---
+++ 04-default-params.js
@@ -0,0 +1,9 @@
+function newUser(name,age,country) {
+    var name = name || 'Oscar'
+    var age = age || 23
+    var country = country || 'Colombia'
+
+    console.log(name,age,country);
+}
+newUser()
+newUser("giovany",27,"CO")
