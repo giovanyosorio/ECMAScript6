38589f8be2c7aade834d577feceade89a3bcd157
gio <giovany.torres.799@gmail.com>
2023-02-03 16:54:28 -0500

    strings

---
+++ 03-strings.js
@@ -0,0 +1,17 @@
+//template literals
+let hello="hello"
+
+let world="world"
+
+
+let saludo =(hello,world) =>{
+    return `${hello} ${world}`
+}
+
+saludo("hola","mundo")
+
+
+//multi-line Strings
+
+let lorem="esto es un string  \n" + " "
+
