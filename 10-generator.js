4b9c1e7a9602d43f0454a2fce3f0e57ece18ae85
gio <giovany.torres.799@gmail.com>
2023-02-06 15:45:56 -0500

    generators

--- 10-generator.js
+++ 10-generator.js
@@ -7,3 +7,15 @@ function* iterate(array) {
 const it=iterate(["hola","mundo","europa","oceania"])
 
 console.log(it.next().value) ;console.log(it.next().value); console.log(it.next().value);console.log(it.next().value);console.log(it.next().value);
+
+function* getId(mumbers) {
+    // Tu código aquí 👈
+    for (let index of mumbers){
+      yield index
+    }
+  }
+  
+  const id = getId([1,2,3])
+  id.next().value
+  id.next().value
+  id.next().value
