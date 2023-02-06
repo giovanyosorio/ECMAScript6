dc2a21c995e71760f178bda2b1487501c4db0f6c
gio <giovany.torres.799@gmail.com>
2023-02-06 10:47:06 -0500

    promises

---
+++ 07-promises.js
@@ -0,0 +1,28 @@
+
+const a =5
+let b=3
+/*
+function sum(num1,num2) {
+    return new Promise((resolve, reject) => {
+        let result = num1+num2
+    }).then(result)
+}
+*/
+
+const suma  =(num1,num2)=> {
+    return new Promise((resolve, reject) => {
+        if (num1>num2) {
+            resolve("es mayor ")
+        }else{
+            reject("no es mayor")
+        }
+    })
+}
+
+
+suma(7,4).then(response=>{
+    console.log(response);
+})
+.catch(reject=>{
+    console.log(reject);
+})
