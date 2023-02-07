e714bcb94b2ac517ab361c372aefd4b969ba5b34
gio <giovany.torres.799@gmail.com>
2023-02-07 11:28:41 -0500

    string-padding and trailing commas

---
+++ ECMAScript8/02-string-padding.js
@@ -0,0 +1,4 @@
+const string ="hello world"
+
+console.log(string.padStart(14,"hi"));
+console.log(string.padEnd(14,"_"));
---
+++ ECMAScript8/03-trailing-commas.js
@@ -0,0 +1,10 @@
+const array=[25,36,78,45,41, ,465 ]
+
+console.log(array);
+console.log(array.length);
+
+const clasesEs8 = ['00', '01', , '03', '04'];
+
+console.log(clasesEs8);
+console.log(clasesEs8.length);
+
