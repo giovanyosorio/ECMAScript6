642b710539440b368450f54fa3b7494604618239
gio <giovany.torres.799@gmail.com>
2023-02-08 11:16:12 -0500

    optional chaining ?

---
+++ ES11/01-optional-chaining.js
@@ -0,0 +1,13 @@
+const users ={
+
+    go:{
+        country:"CO",
+        age:28
+    },
+    gndx: {
+        country:"EM"
+    }
+}
+
+console.log(users);
+console.log(users?.bebeloper?.country);
