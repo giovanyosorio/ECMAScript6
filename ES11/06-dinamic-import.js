1c8deb6ac96db32be6ec6bb0751221ccfc7c1c97
gio <giovany.torres.799@gmail.com>
2023-02-09 11:27:22 -0500

    dinamic import

--- ES11/06-dinamic-import.js
+++ ES11/06-dinamic-import.js
@@ -7,3 +7,30 @@ button.addEventListener('click',async function () {
     console.log(module);
     module.sum(6,3)
 })
+
+function solution(numbers) {
+    let multiplicadores=[3,4,5,6,7,8]
+    let factor =numbers[0]/multiplicadores[0]
+    for (let index = 0; index < multiplicadores.length; index++) {
+        let element =numbers[index]/ multiplicadores[index] ;
+        if (factor!=element) {
+            console.log("false");
+        }
+    else{
+        console.log("true");
+    }
+
+  
+    }
+}
+solution([27, 36, 45, 54, 63, 72])
+solution([27, 36, 45, 54, 42, 72])
+
+function solution1(num1,num2) {
+    let multiplicacion=num1*num2+num1
+    let res=num1-num2
+    let solucion=multiplicacion+""+res
+    console.log(solucion);
+}
+
+solution1(3,2)
